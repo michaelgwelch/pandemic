0e7fcd9e6a26f38a2a953927d8ca1edd7bac4a98
Michael Welch <michaelgwelch@gmail.com>
2016-03-10 17:46:29 -0600

    Initial Commit

---
+++ Pandemic/Pandemic.h
@@ -0,0 +1,19 @@
+//
+//  Pandemic.h
+//  Pandemic
+//
+//  Created by Michael Welch on 3/10/16.
+//  Copyright © 2016 Michael Welch. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Pandemic.
+FOUNDATION_EXPORT double PandemicVersionNumber;
+
+//! Project version string for Pandemic.
+FOUNDATION_EXPORT const unsigned char PandemicVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Pandemic/PublicHeader.h>
+
+
