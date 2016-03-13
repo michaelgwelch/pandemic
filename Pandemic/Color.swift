ab06c9f5548ede6a7551b06e709dbc53c81ca4f7
Michael Welch <michaelgwelch@gmail.com>
2016-03-13 13:52:25 -0500

    Add documentation for Color.swift

--- Pandemic/Color.swift
+++ Pandemic/Color.swift
@@ -10,16 +10,31 @@ import Foundation
 
 private let _colors:[Color] = [.Black, .Blue, .Red, .Yellow]
 
+/**
+ The colors used to describe cities and diseases.
+*/
 public enum Color {
     case Red
     case Black
     case Yellow
     case Blue
 
+    /**
+     The collection of all `Color` values
+    */
     static var colors:[Color] {
         return _colors
     }
 
+    /**
+     Returns `true` if `self` is a value different from `other`
+     
+     - parameter other: The other value to compare to
+     
+     - note: This is just a convenience for use in lambda expressions because Swift does not yet (as
+             far as I know) provide a way to create sections out of operators like `!=`
+    */
+    @warn_unused_result
     func notEqual(other:Color) -> Bool {
         return self != other
     }
